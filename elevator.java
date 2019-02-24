3390be3f7bb0a9829d54bd6bc28f63817abaaec9
purcelw2 <warren.purcell2@mail.dcu.ie>
2019-02-24 11:54:05 +0000

    learning doc to help with implementing threads

--- elevator.java
+++ elevator.java
@@ -1,10 +1,19 @@
-public class elevator{
-    
+public class elevator extends Thread();
+{
+
     // 10 floor 0 - 9
     // Will make Evlevator starting random later
     // active is a way to make sure it's not in use, need? TBD.
     private int current_floor = 0;
     private boolean active = false;
+
+    // Needed for Thread class 
+    // e.g elevator p = new elevator ();
+    // p.start ();
+    public void run ( )
+	{
+		System.out.println (“Hello from the thread”);
+	}
 
     public void ArrivingGoingFromTo(int atFloor, int toFloor){
 
---
+++ learning_doc.md
@@ -0,0 +1,7 @@
+[Threading vs Runnable](https://stackoverflow.com/questions/541487/implements-runnable-vs-extends-thread-in-java)
+
+[Tutorial on Threading](https://www.tutorialspoint.com/java/lang/java_lang_thread.htm)
+
+[Wait vs Sleep](https://www.baeldung.com/java-wait-and-sleep)
+
+[Lecture 4 notes on Threading](https://www.computing.dcu.ie/~rbrennan/CA4006%20Lecture%204%20A%20Design%20Pattern-based%20Approach%20to%20Concurrency%20-%20part%201.pdf)
