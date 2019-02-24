62a2dd222f3ebd56a665da4cecef1981f6526f8b
purcelw2 <warren.purcell2@mail.dcu.ie>
2019-02-24 11:19:11 +0000

    added basic files

---
+++ elevator.java
@@ -0,0 +1,13 @@
+public class elevator{
+    
+    // 10 floor 0 - 9
+    // Will make Evlevator starting random later
+    // active is a way to make sure it's not in use, need? TBD.
+    private int current_floor = 0;
+    private boolean active = false;
+
+    public void ArrivingGoingFromTo(int atFloor, int toFloor){
+
+    }
+
+}
