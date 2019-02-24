ef635092702b07d0c79345b9b50ad4a8c8c2edfe
purcelw2 <warren.purcell2@mail.dcu.ie>
2019-02-24 19:08:26 +0000

    Change file name and fixing errors

--- Elevator.java
+++ Elevator.java
@@ -1,18 +1,20 @@
-public class elevator extends Thread();
-{
+import java.lang.*;
 
+public class Elevator extends Thread
+{
+  private int current_floor = 0;
+  private boolean active = false;
     // 10 floor 0 - 9
     // Will make Evlevator starting random later
     // active is a way to make sure it's not in use, need? TBD.
-    private int current_floor = 0;
-    private boolean active = false;
-
-    // Needed for Thread class 
+    Elevator(){
+    }
+    // Needed for Thread class
     // e.g elevator p = new elevator ();
     // p.start ();
-    public void run ( )
+    public void run()
 	{
-		System.out.println (“Hello from the thread”);
+		System.out.println ("Hello from the thread");
 	}
 
     public void ArrivingGoingFromTo(int atFloor, int toFloor){
