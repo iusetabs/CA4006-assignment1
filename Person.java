24e5cbf2e07f78150f67ba249355305566c519f0
purcelw2 <warren.purcell2@mail.dcu.ie>
2019-02-24 20:44:41 +0000

    Added get's and set's to person

--- Person.java
+++ Person.java
@@ -1,7 +1,43 @@
-public class Person{
-  public static void main(String args[]) {
-    Elevator e1 = new Elevator();
-    Elevator e2 = new Elevator();
-    e1.start();
+public class Person {
+
+  private Thread t;
+  private String pid;
+  private int cur_floor;
+  private int tar_floor;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getCur_floor() {
+		return cur_floor;
+	}
+
+	public void setCur_floor(int cur_floor) {
+		this.cur_floor = cur_floor;
+	}
+
+	public int getTar_floor() {
+		return tar_floor;
+	}
+
+	public void setTar_floor(int tar_floor) {
+		this.tar_floor = tar_floor;
+	}
+
+  public void run() {
+
   }
+
+   public void start () {
+      System.out.println("Starting " +  pid );
+      if (t == null) {
+         t = new Thread (this, pid);
+         t.start ();
+      }
+   }
 }
