1e7986cd7e2c5e06c723a5b473b1f4eb9e90c37b
purcelw2 <warren.purcell2@mail.dcu.ie>
2019-02-24 19:36:56 +0000

    Person now call Elevator object

--- Elevator.java
+++ Elevator.java
@@ -1,24 +1,22 @@
 import java.lang.*;
 
-public class Elevator extends Thread
-{
+public class Elevator extends Thread{
+
   private int current_floor = 0;
   private boolean active = false;
     // 10 floor 0 - 9
     // Will make Evlevator starting random later
     // active is a way to make sure it's not in use, need? TBD.
-    Elevator(){
-    }
+
+
     // Needed for Thread class
     // e.g elevator p = new elevator ();
     // p.start ();
-    public void run()
-	{
-		System.out.println ("Hello from the thread");
+  public void run(){
+
+	   System.out.println ("Hello from the thread");
 	}
 
-    public void ArrivingGoingFromTo(int atFloor, int toFloor){
-
-    }
-
+  public void ArrivingGoingFromTo(int atFloor, int toFloor){
+  }
 }
---
+++ Person.java
@@ -0,0 +1,7 @@
+public class Person{
+  public static void main(String args[]) {
+    Elevator e1 = new Elevator();
+    Elevator e2 = new Elevator();
+    e1.start();
+  }
+}
