0c1a01a56f8817ae8faf26b848127db9f68592ac
james o'nualláin <james.nolan38@mail.dcu.ie>
2019-03-14 18:37:05 +0000

    Basic queue implemented

--- Scenario.java
+++ Scenario.java
@@ -1,13 +1,22 @@
 import java.util.*;
+import java.util.concurrent.*;
+
 
 public class Scenario{
 
   public static void main(String [] args){
-    ConcurrentLinkedQueue test_queue = new ConcurrentLinkedQueue(); //FIFO not allowed null elements
+    ConcurrentLinkedQueue<Person> test_queue = new ConcurrentLinkedQueue<Person>(); //FIFO not allowed null elements
     //The size function and the any function with All are not guaranted to work. Unless you lock queue during these operations.
     //Don't rely on iterators with this queue. It can concurrently change.
     //https://docs.oracle.com/javase/10/docs/api/java/util/concurrent/ConcurrentLinkedQueue.html
     Elevator elev1 = new Elevator("1");
-    Person p1 = new Person("A", 1, 10);
+    Person p1 = new Person(1, 10);
+
+    //TODO some loop which adds people
+    Person [] people_array = new Person[] {new Person(1, 10), new Person(3, 4), new Person(9, 3), new Person(7,8)};
+    int i = 0;
+    for (Person elem : people_array){
+      test_queue.add(elem);
+    }
   }
 }
